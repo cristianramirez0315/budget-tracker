88c50b8eadc4c0a3a2d7b46df98b61d94e3f704b
Cristian Ramirez <cristianramirez0315@gmail.com>
2022-01-09 00:24:53 -0500

    deploy

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
-console.log('### env', process.env);
-console.log('### mongo', MONGODB_URI);
 
 const app = express();
 
@@ -19,9 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
