425bba8938c42c80a1e84827efce789a6679bccc
Cristian Ramirez <cristianramirez0315@gmail.com>
2022-01-08 20:55:22 -0500

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODBd_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 console.log('### env', process.env);
 console.log('### mongo', MONGODB_URI);
